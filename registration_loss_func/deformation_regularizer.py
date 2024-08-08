510d47ba692daa032cda9b60c4d704ab5a398ae7
Junyu Chen <jchen245@jhmi.edu>
2024-08-08 13:09:43 -0400

    Create deformation_regularizer.py

---
+++ registration_loss_func/deformation_regularizer.py
@@ -0,0 +1,105 @@
+'''
+Image Similarity Measures for Medical Image Registration
+
+Article:
+    A Survey on Deep Learning in Medical Image Registration: New Technologies, Uncertainty, Evaluation Metrics, and Beyond
+    J Chen*, Y Liu*, S Wei*, Z Bian, S Subramanian, A Carass, JL Prince, Y Du
+    arXiv preprint arXiv:2307.15615
+    *: Contributed equally
+
+Contact:
+    Junyu Chen
+    jchen245@jhmi.edu
+    Johns Hopkins University
+'''
+
+import torch, math
+import numpy as np
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.nn as nn
+
+class Grad3d(torch.nn.Module):
+    """
+    Diffusion Regularizer adapted from VoxelMorph
+    """
+
+    def __init__(self, penalty='l1', loss_mult=None):
+        super(Grad3d, self).__init__()
+        self.penalty = penalty
+        self.loss_mult = loss_mult
+
+    def forward(self, y_pred, _):
+        dy = torch.abs(y_pred[:, :, 1:, :, :] - y_pred[:, :, :-1, :, :])
+        dx = torch.abs(y_pred[:, :, :, 1:, :] - y_pred[:, :, :, :-1, :])
+        dz = torch.abs(y_pred[:, :, :, :, 1:] - y_pred[:, :, :, :, :-1])
+
+        if self.penalty == 'l2':
+            dy = dy * dy
+            dx = dx * dx
+            dz = dz * dz
+
+        d = torch.mean(dx) + torch.mean(dy) + torch.mean(dz)
+        grad = d / 3.0
+
+        if self.loss_mult is not None:
+            grad *= self.loss_mult
+        return grad
+
+  class Grad3DiTV(torch.nn.Module):
+    """
+    Isotropic Total Variational Regularizer
+    """
+
+    def __init__(self, loss_mult=None):
+        super(Grad3DiTV, self).__init__()
+        self.loss_mult = loss_mult
+
+    def forward(self, y_pred, _):
+        dy = torch.abs(y_pred[:, :, 1:, 1:, 1:] - y_pred[:, :, :-1, 1:, 1:])
+        dx = torch.abs(y_pred[:, :, 1:, 1:, 1:] - y_pred[:, :, 1:, :-1, 1:])
+        dz = torch.abs(y_pred[:, :, 1:, 1:, 1:] - y_pred[:, :, 1:, 1:, :-1])
+        dy = dy * dy
+        dx = dx * dx
+        dz = dz * dz
+        d = torch.mean(torch.sqrt(dx+dy+dz+1e-6))
+        grad = d / 3.0
+        if self.loss_mult is not None:
+            grad *= self.loss_mult
+        return grad
+
+class BendingEnergy3D(torch.nn.Module):
+    """
+    Bending Energy Regularizer
+    """
+    def __init__(self, loss_mult=None):
+        super().__init__()
+        self.loss_mult = loss_mult
+
+    def gradient_dx(self, fv): return (fv[:, 2:, 1:-1, 1:-1] - fv[:, :-2, 1:-1, 1:-1]) / 2
+
+    def gradient_dy(self, fv): return (fv[:, 1:-1, 2:, 1:-1] - fv[:, 1:-1, :-2, 1:-1]) / 2
+
+    def gradient_dz(self, fv): return (fv[:, 1:-1, 1:-1, 2:] - fv[:, 1:-1, 1:-1, :-2]) / 2
+
+    def gradient_txyz(self, Txyz, fn):
+        return torch.stack([fn(Txyz[:,i,...]) for i in [0, 1, 2]], dim=1)
+
+    def compute_bending_energy(self, displacement):
+        dTdx = self.gradient_txyz(displacement, self.gradient_dx)
+        dTdy = self.gradient_txyz(displacement, self.gradient_dy)
+        dTdz = self.gradient_txyz(displacement, self.gradient_dz)
+        dTdxx = self.gradient_txyz(dTdx, self.gradient_dx)
+        dTdyy = self.gradient_txyz(dTdy, self.gradient_dy)
+        dTdzz = self.gradient_txyz(dTdz, self.gradient_dz)
+        dTdxy = self.gradient_txyz(dTdx, self.gradient_dy)
+        dTdyz = self.gradient_txyz(dTdy, self.gradient_dz)
+        dTdxz = self.gradient_txyz(dTdx, self.gradient_dz)
+        return torch.mean(dTdxx**2 + dTdyy**2 + dTdzz**2 + 2*dTdxy**2 + 2*dTdxz**2 + 2*dTdyz**2)
+
+    def forward(self, disp, _):
+        energy = self.compute_bending_energy(disp)
+        if self.loss_mult is not None:
+            energy *= self.loss_mult
+        return energy
+    
